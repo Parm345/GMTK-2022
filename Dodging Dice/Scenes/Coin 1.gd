f434ec48f87292896491cd58a234b065e3916c6d
Xiangyu Chen <chenxy327@gmail.com>
2022-07-15 16:30:30 -0400

    scaled coin collider with animation

--- Dodging Dice/Scenes/Coin 1.gd
+++ Dodging Dice/Scenes/Coin 1.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 onready var collider:Node = $"CollisionShape2D";
 onready var animation:Node = $"AnimatedSprite";
+var WIDTH:float = 4.0;
+var DIAMETER:float = 16.0;
 
 func _process(delta):
-	collider.scale.x = animation.frame;
+	collider.scale.x = WIDTH/DIAMETER + abs(sin(animation.frame/32.0*PI))*(DIAMETER-WIDTH)/DIAMETER;
