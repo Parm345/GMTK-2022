4055d71ec58c5e8e98117a853c21e1f52b8ce0ef
Xiangyu Chen <chenxy327@gmail.com>
2022-07-15 18:04:43 -0400

    buggy tilemap breaking

--- Dodging Dice/Scenes/Dice Platform.gd
+++ Dodging Dice/Scenes/Dice Platform.gd
@@ -1,8 +1,10 @@
 extends TileMap
 
 onready var player:Node = $"../Player";
-var solid_time:float = 0.4;
+var timer = preload("res://Scenes/Timer (Oneshot).tscn");
+var solid_time:float = 0.6;
 const NOTHING:int = -1;
+var breakings = [];
 
 func _ready():
 	player.connect("collided", self, "_on_Player_collided");
@@ -13,4 +15,15 @@ func _on_Player_collided(collision):
 		var collision_pos:Vector2 = world_to_map(collision.position - collision.get_normal());
 		var type:int = get_cell(collision_pos.x, collision_pos.y);
 		if type >= 1 && type <= 6:
-			set_cell(collision_pos.x, collision_pos.y, NOTHING);
+			breakings.append(collision_pos);
+			start_timer(solid_time);
+
+func start_timer(sec):
+	var instance:Node = timer.instance();
+	instance.connect("timeout", self, "_on_Timer_timeout");
+	add_child(instance);
+	instance.start(sec);
+
+func _on_Timer_timeout():
+	var tile_position:Vector2 = breakings.pop_front();
+	set_cell(tile_position.x, tile_position.y, NOTHING);
