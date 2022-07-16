bf29814778eeac6e736cb56f6ff185f6211f20c1
Parm345 <51205078+Parm345@users.noreply.github.com>
2022-07-16 12:20:16 -0400

    Die can be stopped now

--- Dodging Dice/Dice/Snapping Dice.gd
+++ Dodging Dice/Dice/Snapping Dice.gd
@@ -1,14 +1,17 @@
 extends StaticBody2D
 
-export var speed = 3
+export var speed = 0.1
 
 var dieValue = 3
 var tileSize = 32
 var castDirection: Vector2 = Vector2()
 var tilesMoved = dieValue
 var isMoving = false
+var checkTile = false
+var movedAlongTile = 0
 
 onready var ray = $RayCast2D
+onready var ray2 = $RayCast2D2
 onready var tween = $Tween
 
 # Called when the node enters the scene tree for the first time.
@@ -17,6 +20,7 @@ func _ready():
 	position += Vector2.ONE * tileSize/2 # makes sure that the player is always centered
 
 func playerCollision(playerFacingDirection:Vector2):
+#	position = position.snapped(Vector2.ONE * tileSize)
 	if !isMoving:
 		ray.cast_to = playerFacingDirection * dieValue * tileSize
 		ray.force_raycast_update()
@@ -24,28 +28,38 @@ func playerCollision(playerFacingDirection:Vector2):
 			castDirection = playerFacingDirection
 			isMoving = true
 		tilesMoved = 0
+		movedAlongTile = 0
 #		get_tree().paused = true
 
 func moveTween():
 	tween.interpolate_property(self, "position", position, position + castDirection * tileSize, 
-		1.0/speed, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+		3, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
 	tween.start()
 	
 func move():
 	ray.cast_to = castDirection * tileSize
+	ray2.cast_to = castDirection * 17
 	ray.force_raycast_update()
-	if !ray.is_colliding() and tilesMoved < dieValue and isMoving:
-		position += castDirection * tileSize
-#		moveTween()
-		tilesMoved += 1
+	ray2.force_raycast_update()
+	
+	if !ray.is_colliding():
+		checkTile = true
+	if !ray2.is_colliding() and tilesMoved < dieValue and isMoving and checkTile:
+		movedAlongTile += speed
+		position += castDirection * speed
+		if movedAlongTile >= tileSize:
+			tilesMoved += 1
+			movedAlongTile = 0
+			print(tilesMoved)
+#			get_tree().paused = true
 	if ray.is_colliding():
 		tilesMoved = dieValue
-		print("yo")
 
 	
 	if tilesMoved == dieValue:
 		castDirection = Vector2()
 		isMoving = false
+		position = position.snapped(Vector2.ONE * tileSize)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
