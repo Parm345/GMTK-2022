fb374cd3996627c35c097c1efdf224a6fb944db5
Xiangyu Chen <chenxy327@gmail.com>
2022-07-15 22:36:52 -0400

    sliding die half working

--- Dodging Dice/Scenes/Sliding Die.gd
+++ Dodging Dice/Scenes/Sliding Die.gd
@@ -1,10 +1,13 @@
 extends KinematicBody2D
 
-var t:float = 32;
+var tile_size:float = 32;
 var value:int = 1;
-var speed:float = 128;
 var dice_sprites = [];
 var distance:float = 0;
+var speed:float = 32;
+var velocity:Vector2 = Vector2(0, 0);
+var target_position:Vector2 = Vector2(0, 0);
+var sliding:bool = false;
 
 func _ready():
 	for i in range(7):
@@ -12,7 +15,23 @@ func _ready():
 		dice_sprites.append(sprite);
 	dice_sprites[value].visible = true;
 
-func move(direction):
-	var velocity:Vector2 = speed*direction;
-	velocity = move_and_slide(velocity, Vector2(0,0));
+func _physics_process(delta):
+	if sliding:
+		if are_equal_approx(global_position, target_position):
+			global_position = target_position;
+			sliding = false;
+		else:
+			velocity = move_and_slide(velocity, Vector2(0, 0));
 		
+
+func move(direction):
+	if sliding:
+		return;
+	target_position = global_position + direction*value*tile_size;
+	velocity = speed*direction;
+	sliding = true;
+		
+func are_equal_approx(position1, position2):
+	if abs(position1.x-position2.x) < 1 && abs(position1.y-position2.y) < 1:
+		return true;
+	return false;
