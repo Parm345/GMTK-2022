d9e6d4ce45a0c6127bc77a48086882805ef1627f
Parm345 <51205078+Parm345@users.noreply.github.com>
2022-07-16 15:26:33 -0400

    added animation to player

--- Dodging Dice/Player/Player 2.gd
+++ Dodging Dice/Player/Player 2.gd
@@ -24,8 +24,15 @@ func _physics_process(delta):
 	#if is_on_floor():
 	if Input.is_action_pressed("ui_left"):
 		velocity.x -= hforce;
+		$AnimatedSprite.play("run")
+		$AnimatedSprite.flip_h = true
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += hforce;
+		$AnimatedSprite.play("run")
+		$AnimatedSprite.flip_h = false
+	if Input.is_action_just_released("ui_left") or Input.is_action_just_released("ui_right"):
+		velocity.x = 0
+		$AnimatedSprite.play("idle")
 	
 	#gravity
 	velocity.y += mass*delta;
