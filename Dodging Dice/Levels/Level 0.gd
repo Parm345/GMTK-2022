6429fee8374f0c6cde3cf25c21ed9d5552204f7f
Xiangyu Chen <chenxy327@gmail.com>
2022-07-17 13:16:21 -0400

    added music to level 0

--- Dodging Dice/Levels/Level 0.gd
+++ Dodging Dice/Levels/Level 0.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 onready var game:Node = $"/root/Game";
+onready var music:Node = $"Ditto Fast";
 const TUTORIAL:int = -1;
 
 func _on_Start_Button_button_up():
@@ -11,3 +12,6 @@ func _on_Start_Button_button_up():
 func _on_Tutorial_Button_button_up():
 	game.add_level(TUTORIAL);
 	queue_free();
+
+func _on_Ditto_Fast_finished():
+	music.play();
