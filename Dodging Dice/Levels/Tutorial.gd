acde865560d779fa68f61f3caab1318861782d61
Xiangyu Chen <chenxy327@gmail.com>
2022-07-17 11:38:53 -0400

    save button works

--- Dodging Dice/Levels/Tutorial.gd
+++ Dodging Dice/Levels/Tutorial.gd
@@ -1,15 +1,19 @@
 extends Node2D
 
 onready var game:Node = $"/root/Game";
+onready var player:Node = $"Player 2";
 
 func _on_Restart_Button_button_up():
+	player.get_node("CollisionShape2D").set_disabled(true);
 	game.add_level(game.current_level);
 	queue_free();
 
 func _on_Home_Button_button_up():
+	player.get_node("CollisionShape2D").set_disabled(true);
 	game.add_level(0);
 	queue_free();
 
 func _on_Button_pressed():
+	player.get_node("CollisionShape2D").set_disabled(true);
 	game.add_level(0);
 	queue_free();
