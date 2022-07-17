18a0922d26a6ae75d21d41379c67ed0dfef6444d
Xiangyu Chen <chenxy327@gmail.com>
2022-07-17 11:49:02 -0400

    R for restart

--- Dodging Dice/Levels/Level 1.gd
+++ Dodging Dice/Levels/Level 1.gd
@@ -3,6 +3,12 @@ extends Node2D
 onready var game:Node = $"/root/Game";
 onready var player:Node = $"Player 2";
 
+func _input(event):
+	if event.is_action_pressed("ui_restart"):
+		player.get_node("CollisionShape2D").set_disabled(true);
+		game.add_level(game.current_level);
+		queue_free();
+
 func _on_Restart_Button_button_up():
 	player.get_node("CollisionShape2D").set_disabled(true);
 	game.add_level(game.current_level);
