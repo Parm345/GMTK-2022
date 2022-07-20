69792d3c1f23e0b313aaf1d4278a50caf123846b
Xiangyu Chen <chenxy327@gmail.com>
2022-07-20 16:29:29 -0400

    testing

--- Dodging Dice/Player/player_variables.gd
+++ Dodging Dice/Player/player_variables.gd
@@ -4,3 +4,7 @@ var level_coin_count:int = 0;
 var total_coin_count:int = 0;
 var position_saved:bool = false;
 var saved_position:Vector2 = Vector2(0, 0);
+
+func push_coin_count():
+	total_coin_count += level_coin_count;
+	level_coin_count = 0;
